b9a36bdbd9eec0199a6d99bf836a15626471bd8a
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2025-09-05 10:20:13 +0100

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 from datetime import datetime
+import os
 
 app = Flask(__name__)
+CORS(app)  # allow frontend calls
 DB = "payme.db"
 
 
@@ -156,4 +159,5 @@ def transactions(user_id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
