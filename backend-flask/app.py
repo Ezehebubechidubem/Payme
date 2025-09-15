d0c80f9f3ee4feb95bc87afbaf801bf9c03cb5c1
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2025-09-15 22:39:34 +0100

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -6,7 +6,7 @@ import os
 import sys
 import traceback
 import requests
-
+import math
 # -------------------------------------------------
 # Postgres Support
 # -------------------------------------------------
