5f4b2675e262af3a795a805b2cb30b6ce19ed5f2
Ezehebubechidubem <140657875+Ezehebubechidubem@users.noreply.github.com>
2025-11-29 16:55:28 +0100

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -155,19 +155,14 @@ class PGConnectionContext:
             pass
 
 def get_conn():
-    """
-    Returns a valid DB connection:
-    - PGConnectionContext for Postgres
-    - SQLite connection for SQLite
-    """
     if DATABASE_URL:
         return PGConnectionContext(DATABASE_URL)
     else:
         conn = sqlite3.connect(DB, check_same_thread=False)
         conn.row_factory = sqlite3.Row
-        # DO NOT use "with conn:" here
-        conn.execute("PRAGMA journal_mode=WAL;")
-        conn.execute("PRAGMA foreign_keys=ON;")
+        with conn:
+            conn.execute("PRAGMA journal_mode=WAL;")
+            conn.execute("PRAGMA foreign_keys=ON;")
         return conn
 
 def init_db():
